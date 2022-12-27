446e1185fbdf0dae0ce34f55281e6a2101c16733
Brad Coleman <bradcoleman60@gmail.com>
2022-12-26 21:07:15 -0800

    adds radio selector and function to compare to an answer bank

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ function setTimer() {
 var questionDom = document.querySelector(".question-box");
 var answerAdom = document.querySelector(".answer-box")
 var questionBank = [["Question 1 :   What is Javascript?","A - a coffrer","B - a car","C - a plane ","D - non of the above "],["Question 2:    What is MYSQL?","A - A plane ","B - a system of items","C - absolutely nothing ","D - All of the above "],["Question 3:    How is an Array different than an Object?","A - An array is longer","B - An object is a noun","C - they are the same","D - An array is a ray of light"],["Question 4:    What does the method BIKE do?","A - Assigns a value to a variable","B - Takes mom to lunch","C - Goes on a bike ride ","D - Never eats at McDonalds"]];
-var questionNumber = 3;
+var questionNumber = 2;
 
 questionDom.children[0].textContent = questionBank[questionNumber][0];
 answerAdom.children[0].textContent = questionBank[questionNumber][1];
@@ -72,26 +72,28 @@ console.log(welcomeDom)
 /////////////////////////////////////////////////////////////////////////////
 
 
-//////////////////////Testing of radio button submit feature/////////////////////
+//////////////////////Gets value of selected radio and compares to the answer bank /////////////////////
+
+var answerBank = ["a","b","c","d"];
 
 function radiofunction(){
-    var choiceMade = document.getElementsByName('test-box');
-
-    for (i=0; i < choiceMade.length; i++){
-        if(brad[i].checked)
-        document.getElementById("result").innerHTML = "The answer is:" + choiceMade[i].value;
         
-    }
-    console.log("brad length:  " + choiceMade.length)
-        console.log(choiceMade)
-        console.log(choiceMade[0].checked)
-        console.log(choiceMade[1].checked)
-        console.log(choiceMade[2].checked)
-        console.log(choiceMade[3].checked)
+    var actualAnswerSelected = document.querySelector('input[name="test-box"]:checked').value;
+
+    console.log("THIS IS IT: " + actualAnswerSelected)
+    document.getElementById("result").innerHTML = actualAnswerSelected;
+
+    if (actualAnswerSelected === answerBank[0])
+    {
+        console.log("YOU DID IT")
+    } else {
+        console.log("YOU CRAPPED OUT")
+    };
+
+
 }
 
-var brad = document.getElementsByName("test-box");
-console.log(brad.length)
 
 ///////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
+
