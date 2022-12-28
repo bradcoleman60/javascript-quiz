aeaff2bf5b662a6c8cffb217177a6a7a00647c21
Brad Coleman <bradcoleman60@gmail.com>
2022-12-28 05:23:12 -0800

    removes all [-1] from tags and variables after testing new approach to radio dials. No new functionality added in this commit - just cleans all internal references

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,16 +23,18 @@ function setTimer() {
 
 //This JS sets the question and answer array and pushes the sets of questions and answers to the webpage based on the questionNumber1 variable////////
 
-var questionDom1 = document.querySelector(".question-box-1");
-var answerAdom1 = document.querySelector(".submission-form-1")
-var questionBank1 = [["Question 1 :   What is Javascript?","A - a coffee","B - a car","C - a plane ","D - non of the above "],["Question 2:    What is MYSQL?","A - A plane ","B - a system of items","C - absolutely nothing ","D - All of the above "],["Question 3:    How is an Array different than an Object?","A - An array is longer","B - An object is a noun","C - they are the same","D - An array is a ray of light"],["Question 4:    What does the method BIKE do?","A - Assigns a value to a variable","B - Takes mom to lunch","C - Goes on a bike ride ","D - Never eats at McDonalds"]];
-var questionNumber1 = 2;
+var questionDom = document.querySelector(".question-box");
+var answerAdom = document.querySelector(".submission-form")
+var questionBank = [["Question 1 :   What is Javascript?","A - a coffee","B - a car","C - a plane ","D - non of the above "],["Question 2:    What is MYSQL?","A - A plane ","B - a system of items","C - absolutely nothing ","D - All of the above "],["Question 3:    How is an Array different than an Object?","A - An array is longer","B - An object is a noun","C - they are the same","D - An array is a ray of light"],["Question 4:    What does the method BIKE do?","A - Assigns a value to a variable","B - Takes mom to lunch","C - Goes on a bike ride ","D - Never eats at McDonalds"]];
+var questionNumber = 0;
 
-questionDom1.children[0].textContent = questionBank1[questionNumber1][0];
-answerAdom1.children[1].textContent = questionBank1[questionNumber1][1];
-answerAdom1.children[4].textContent = questionBank1[questionNumber1][2];
-answerAdom1.children[7].textContent = questionBank1[questionNumber1][3];
-answerAdom1.children[10].textContent = questionBank1[questionNumber1][4];
+
+
+questionDom.children[0].textContent = questionBank[questionNumber][0];
+answerAdom.children[1].textContent = questionBank[questionNumber][1];
+answerAdom.children[4].textContent = questionBank[questionNumber][2];
+answerAdom.children[7].textContent = questionBank[questionNumber][3];
+answerAdom.children[10].textContent = questionBank[questionNumber][4];
 
 
 //Sets start quiz function.  This removes the welcome message and start quiz button and starts the quiz./////// 
@@ -40,7 +42,7 @@ var welcomeDom = document.querySelector(".welcome-message")
 
 function startQuiz() {
 welcomeDom.setAttribute("style", "display: none");
-questionDom1.setAttribute("style", "display: block");
+questionDom.setAttribute("style", "display: block");
 setTimer()
 }
 
@@ -52,18 +54,23 @@ function endOfQuizMessage(){
 };
 
 
-//////////////////////Gets value of selected radio and compares to the answer bank /////////////////////
+//Gets value of selected radio and compares to the answer bank /////////////////////
 
 var answerBank = ["a","b","c","d"];
 
-function radiofunction(){
+function submitAnswer(){
         
-    var actualAnswerSelected = document.querySelector('input[name="test-box-1"]:checked').value;
+    var actualAnswerSelected = document.querySelector('input[name="test-box"]:checked').value;
 
+    console.log("questionNumber = " + questionNumber);
     console.log("THIS IS IT: " + actualAnswerSelected)
     document.getElementById("result").innerHTML = actualAnswerSelected;
+    questionNumber++;
+    questionAnswerBox();
+    console.log("new number for question number :  " +questionNumber);
+    
 
-    if (actualAnswerSelected === answerBank[questionNumber1])
+    if (actualAnswerSelected === answerBank[questionNumber])
     {
         console.log("YOU DID IT")
     } else {
