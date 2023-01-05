35a77345d2d092ed06e5e5725cf1ed187c127d6c
Brad Coleman <bradcoleman60@gmail.com>
2023-01-05 12:22:19 -0800

    reverts code back to web api and removes any jquery for grading purposes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,70 @@
 //Global variables
 
 //Set the time alotment in seconds
-var secondsLeft = 1000
+var secondsLeft = 100
 
 //This array holds the questions position[0], the 5 possible answers (positions [1-4] and the actual answer (position[5])
-var questionBank = [["Question 1 :   Javascript was first known as?"," SuperScript","LiveScript","Netscape Scripter","none of the above ","b"],["Question 2:    All of the following are benefits of using JavaScript except:"," Less server traffic","Visitors do not have to wait for the page to reload","Increased interactivity like reactions when users hover over an element","Supports multithreading","d"],["Question 3:    What tag is used to link a JavaScript file to a website?"," <script>….</script>","<script>","<Jscript>…</Jscript>","<JayScripter>…</JayScripter","a"],["Question 4:    Which of the following are the two scopes variables can have in JavaScript?"," Intenational and local","Large and small","Global and local","Global and remote","c"],["Question 5:    All of the following are primitive data types in JavaScript except"," Numbers","Strings","Boolean","Null","d"],["Question 6:    Which of the following is a reserved word in JavaScript?"," Volatility","Case","Continuation","without","b"],["Question 7:    Which of the following is not a supported opeator in JavaScript?"," Arithmetic Operators","Comparison Operators","Smooth Operators","Logical Operators","c"],["Question 8:    What is a function in JavaScript?"," Built-on code snippets that can be used in your program","Arithemetic calculators like sum and average","A group of reusable code that can can called anywhere in your program","An event ","c"],["Question 9:    A return statement in a function is:"," Always required","Only required if a FOR LOOP is used in the function","Never included in a function","Required if you want to return a value from the function","d"],["Question 10:    Which of the following is a string method?"," CharAt()","pop()","map()","unshift()","a"],
-];
+var questionBank = [["Question 1 :   Javascript was first known as?","A - SuperScript","B - LiveScript","C - Netscape Scripter","D - none of the above ","b"],["Question 2:    All of the following are benefits of using JavaScript except:","A - Less server traffic","B - Visitors do not have to wait for the page to reload","C - Increased interactivity like reactions when users hover over an element","D - Supports multithreading","d"],["Question 3:    What tag is used to link a JavaScript file to a website?","A - <script>….</script>","B - <script>","C - <Jscript>…</Jscript>","D - <Scripter>…</Scripter","a"],["Question 4:    Which of the following are the two scopes that variables can have in JavaScript?","A - Intenational and local","B - Large and small","C - Global and local","D - Global and remote","c"],["Question 5:    All of the following are primitive data types except","A - Numbers","B - Strings","C - Boolean","D - Null","d"],["Question 6:    Which of the following is a reserved word?","A - Volatility","B - Case","C - Continuation","D - without","b"],["Question 7:    Which of the following is not a supported opeator in JavaScript?","A - Arithmetic Operators","B - Comparison Operators","C - Smooth Operators","D - Logical Operators","c"],["Question 8:    What is a function in JavaScript?","A - Built-on code snippets that can be used in your program","B - Arithemetic calculators like sum and average","C - A group of reusable code that can can called anywhere in your program","D - An event ","c"],["Question 9:    A return statement in a function is:","A - Always required","B - Only required if a FOR LOOP is used in the function","C - Never included in a function","D - Required if you want to return a value from the function","d"],["Question 10:    Which of the following is a string method?","A - CharAt()","B - pop()","C - map()","D - unshift()","a"]];
 
 //Other global variable
 var questionNumber = 0;
+var countDownDom = document.querySelector(".countdown")
 var scoreCounter = 0;
 var answeredQuestions = 0;
+var questionDom = document.querySelector(".question-box");
 
 //This function creates the coundwon timer
 function setTimer() {
-      var timerInterval = setInterval(function() {
+    var initialsDom = document.querySelector(".initialsInput");
+    var timerInterval = setInterval(function() {
       secondsLeft--;
       //This IF statement ends the quiz of the secondsLeft is zero or all question have been answered  
       if(secondsLeft <= 0 || answeredQuestions === questionBank.length) {
       clearInterval(timerInterval)
             endOfQuizMessage();
-            $(".question-box").attr("style", "display: none");
-            $(".initialsInput").attr("style", "display: flex");
+            questionDom.setAttribute("style", "display: none");
+            initialsDom.setAttribute("style", "display: flex");
              }
         
-             $("#counter").text(secondsLeft);
-    
+        document.getElementById('counter').innerHTML =  secondsLeft;
         
   }, 1000);
 }
 
 //Sets start quiz function.  This removes the welcome message and start quiz button and starts the quiz.
+function startQuiz() {
 
-$(".welcome-message").click(function(){
-    $(".welcome-message").attr("style", "display: none");
-    $(".question-box").attr("style", "display: flex");
-    $(".countdown").attr("style", "display: block")
-setTimer();
-displayQuestionBox();
+    var welcomeDom = document.querySelector(".welcome-message")
+    welcomeDom.setAttribute("style", "display: none");
+    questionDom.setAttribute("style", "display: flex");
+    countDownDom.setAttribute("style", "display: block")
+    setTimer();
+    displayQuestionBox();
 
-});
+};
 
 //This function displays the question and multiple answers based on the question number. 
 function displayQuestionBox (){
-       
-    $("p.the-question").text(questionBank[questionNumber][0]);
-    $("li:eq(0)").text(questionBank[questionNumber][1]);
-    $("li:eq(1)").text(questionBank[questionNumber][2]);
-    $("li:eq(2)").text(questionBank[questionNumber][3]);
-    $("li:eq(3)").text(questionBank[questionNumber][4]);
+    
+    var answerDom = document.querySelector(".submission-form");
+    questionDom.children[0].textContent = questionBank[questionNumber][0];
+    answerDom.children[1].textContent = questionBank[questionNumber][1];
+    answerDom.children[4].textContent = questionBank[questionNumber][2];
+    answerDom.children[7].textContent = questionBank[questionNumber][3];
+    answerDom.children[10].textContent = questionBank[questionNumber][4];
 };
+
 
 //This function adds the end of quiz messgae after the timer reaches the end
 function endOfQuizMessage(){
-    
-    $("#end-of-quiz-message").text("Thanks for taking the quiz. Your Score was: " + scoreCounter + " out of " + questionBank.length + ".")
-    $(".countdown").attr("style", "display: none");
-    };
+    document.getElementById('end-of-quiz-message').innerHTML = "Thanks for taking the quiz. Your Score was: " + scoreCounter + " out of " + questionBank.length + ".";
+    countDownDom.setAttribute("style", "display: none");
+};
 
 //This function obtains value of selected radio input and compares to the answer bank
+function submitAnswer(){
 
-$("li").click(function(){
-
-    var actualAnswerSelected = $(this).attr("value");
+    var actualAnswerSelected = document.querySelector('input[type="radio"][name="answer-choice"]:checked').value;
     
     if (actualAnswerSelected === questionBank[questionNumber][5]){
         scoreCounter++;
@@ -73,49 +75,49 @@ $("li").click(function(){
         answeredQuestions++;
     };
     
+
     if (answeredQuestions < questionBank.length){
         questionNumber++;
         displayQuestionBox();
     }
-    
-});
+    var touncheckRadio = document.querySelector('input[type="radio"][name="answer-choice"]:checked');
+    touncheckRadio.checked = false;
+};
 
 //This function allows user to input initials at the completion of the quiz
 var existingleaderBoard = JSON.parse(localStorage.getItem("leaderBoard"));
 
-$("#initials-submit-btn").click(function(){
-    var initialsThatWereInput =$("#initials-submit-text").val();
+function initialsInput(){
+
+    var initialsThatWereInput = document.querySelector('input[type="text-box"][name="initials-input"]').value;
     var newScore = {"initials" : initialsThatWereInput, "score" : scoreCounter , "percentage": (scoreCounter /= questionBank.length).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0})};
 
     //This checks to ensure that letters are input and that the string is not a number
     if(initialsThatWereInput.length === 0 || !isNaN(initialsThatWereInput)){
-    alert("Please enter your initials");
+        alert("Please enter your initials");
     } else {
- 
+     
     //This checks to see if local storage has an existing leaderboard, if not an array 
     //is created to hold the leaderboard    
     if(existingleaderBoard == null) existingleaderBoard = [];
-
+    
     //This pushes the new score into the existing leaderboard
     existingleaderBoard.push(newScore);
     localStorage.setItem("leaderBoard", JSON.stringify(existingleaderBoard));
     existingleaderBoard.sort((a,b)=> (a.score - b.score));
-    displayLeaderboard();
+        displayLeaderboard();
+    }
 }
-}
-    
-);
 
 //This function creates and displays the leaderboard 
+var initialsDom = document.querySelector(".initialsInput");
+var leaderDom = document.querySelector(".leaderboard-table")
 
 function displayLeaderboard(){
     //This removes the intials input box and submit button
-    
-    $(".initialsInput").attr("style", "display: none");
-    
+    initialsDom.setAttribute("style", "display: none");
     //This displays the leaderboard
-    $(".leaderboard-table").attr("style", "display: flex");
-
+    leaderDom.setAttribute("style", "display: flex");
     var table = document.getElementById("leaderboard-table");
 
     //This FOR loop iterates around the locally stored leaderboard and creates a table
@@ -133,8 +135,8 @@ function displayLeaderboard(){
 }
 
 //This function reloards the page to get back to the take the quiz 
-$("#back-to-start").click (function (){
+function backToStart(){
         location.reload();
-});
+}
 
 // localStorage.clear();
